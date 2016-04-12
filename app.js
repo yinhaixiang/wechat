035efc2d1258f524d70521464f29d842c551fdc9
huiyang <haixiang.yhx@alibaba-inc.com>
2016-04-12 19:11:48 +0800

    update

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 
 var Koa = require('koa');
 var sha1 = require('sha1');
+var wechat = require('./wechat');
 
 var config = {
   wechat: {
-    appId: 'wx8eb5b980e2cde7ba',
+    appID: 'wx8eb5b980e2cde7ba',
     appSecret: 'c63eacff58e5adffc1584c9d83bb0c39',
     token: 'yinhaixiang'
   }
@@ -13,22 +14,7 @@ var config = {
 
 var app = new Koa();
 
-app.use(function *(next) {
-  console.log(this.query);
-
-  var token = config.wechat.token;
-  var signature = this.query.signature;
-  var nonce = this.query.nonce;
-  var timestamp = this.query.timestamp;
-  var echostr = this.query.echostr;
-  var str = [token, timestamp, nonce].sort().join('');
-  var sha = sha1(str);
-  if(sha === signature) {
-    this.body = echostr + '';
-  } else {
-    this.body = 'wrong';
-  }
-});
+app.use(wechat(config.wechat));
 
 app.listen(1234);
 
